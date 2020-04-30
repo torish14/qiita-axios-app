3acb4dba8e0430d590adc03580b0c05aaac62be2
toshihirokato <nanamin.104@icloud.com>
2020-05-01 01:39:13 +0900

    ~/plugins/axios.js と環境変数の追記

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,11 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/axios.js'
   ],
+  env: {
+    QIITA_TOKEN: process.env.QIITA_TOKEN
+  },
   /*
   ** Nuxt.js dev-modules
   */
