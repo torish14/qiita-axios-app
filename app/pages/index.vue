caf2665f55b39a9d183ca896f76680e383c508d1
toshihirokato <nanamin.104@icloud.com>
2020-05-01 01:19:04 +0900

    axios を用いて qiita の nuxt.js の投稿を取得

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,77 +1,23 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        qiita-axios-app
-      </h1>
-      <h2 class="subtitle">
-        My sensational Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div />
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  //! async await で簡潔に非同期処理を実装
+  // ? https://qiita.com/soarflat/items/1a9613e023200bbebcb3
+  // async は非同期関数を定義する
+  async mounted () {
+    // eslint-disable-next-line
+    console.log(
+      JSON.stringify(await // await は結果が返されるまで待機する
+      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+      ), true, ''
+    )
   }
 }
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
