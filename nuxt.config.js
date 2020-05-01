6c8fafa4cdba5f88833c934aa1a140aa5e51b737
toshihirokato <nanamin.104@icloud.com>
2020-05-01 13:37:42 +0900

    HTMLタグ設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'qiita-axios-app',
+    titleTemplate: '5s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
