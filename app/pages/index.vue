1a3800b1e52de184ddca7c4190ae251b13a54e51
toshihirokato <nanamin.104@icloud.com>
2020-05-01 12:53:38 +0900

    jsonデータをブラウザに表示

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div />
+  <section class="container">
+    <!-- <div> -->
+    <header>
+      <h1>Nuxt.js のタグが付けられた投稿一覧</h1>
+    </header>
+    <main>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <a :href="item.url"><span>{{ item.title.slice(0,70) }}</span></a>
+            <small>by {{ item.user.id }}</small>
+          </h4>
+          <div class="body">
+            {{ item.body.slice(0, 80) }}・・・
+          </div>
+        </li>
+      </ul>
+    </main>
+    <!-- </div> -->
+  </section>
 </template>
 
 <script>
@@ -7,17 +26,55 @@ export default {
   //! async await で簡潔に非同期処理を実装
   // ? https://qiita.com/soarflat/items/1a9613e023200bbebcb3
   // async は非同期関数を定義する
-  async mounted () {
-    // eslint-disable-next-line
-    console.log(
-      JSON.stringify(await // await は結果が返されるまで待機する
-      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
-      ), true, ''
-    )
+  // async mounted () {
+  // eslint-disable-next-line
+    // console.log(
+  // JSON.stringify(await // await は結果が返されるまで待機する
+  // this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+  // ), true, ''
+  // )
+  // }
+  //! asyncDataメソッドは、サーバーサイドでデータを取得してレンダリングする場合に用いる
+  async asyncData ({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
 
 <style>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
 
+h1 {
+  margin: 16px;
+  padding: 12px;
+  border-bottom: solid 1px #e5e5e5;
+  background-color: #007acc;
+  width: 1160px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+ul {
+  margin-left: 16px;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+span {
+  padding-left: 4px;
+  color: rgba(0, 122, 204, 1);
+}
+
+.body {
+  font-size: 12px;
+}
 </style>
